6e85a07a624df42fdfced2bcdf30db47f05797d4
Thrrmpd <tharamaecorpuz05@gmail.com>
2025-10-02 16:14:17 +0800

    feat: add likePost feature

--- node-unit-mvc/controllers/post.controller.js
+++ node-unit-mvc/controllers/post.controller.js
@@ -1,66 +1,74 @@
-const PostModel = require('../models/post.model');
+const PostModel = require("../models/post.model");
 const PostController = {};
 
 PostController.create = (req, res) => {
-    return PostModel.createPost(req.body, (err, post) => {
-        if (err) {
-            return res.status(500).end();
-        } else {
-            return res.json(post);
-        }
-    })
-
+  return PostModel.createPost(req.body, (err, post) => {
+    if (err) {
+      return res.status(500).end();
+    } else {
+      return res.json(post);
+    }
+  });
 };
 
 PostController.update = (req, res) => {
-    const { id } = req.params;
-    PostModel.updatePost(id, req.body, (err, updatedPost) => {
-        if (err) {
-            return res.status(500).end();
-        }
-        if (!updatedPost) {
-            return res.status(404).end();
-        }
-        return res.json(updatedPost);
-    });
+  const { id } = req.params;
+  PostModel.updatePost(id, req.body, (err, updatedPost) => {
+    if (err) {
+      return res.status(500).end();
+    }
+    if (!updatedPost) {
+      return res.status(404).end();
+    }
+    return res.json(updatedPost);
+  });
 };
 
 PostController.findPost = (req, res) => {
-    const { id } = req.params;
-    PostModel.findPost(id, (err, post) => {
-        if (err) {
-            return res.status(500).end();
-        }
-        if (!post) {
-            return res.status(404).end();
-        }
-        return res.json(post);
-    });
+  const { id } = req.params;
+  PostModel.findPost(id, (err, post) => {
+    if (err) {
+      return res.status(500).end();
+    }
+    if (!post) {
+      return res.status(404).end();
+    }
+    return res.json(post);
+  });
 };
 
 PostController.getAllPosts = (req, res) => {
-    PostModel.getAllPosts((err, posts) => {
-        if (err) {
-            return res.status(500).end();
-        }
-        if (!posts) {
-            return res.status(404).end();
-        }
-        return res.json(posts);
-    });
+  PostModel.getAllPosts((err, posts) => {
+    if (err) {
+      return res.status(500).end();
+    }
+    if (!posts) {
+      return res.status(404).end();
+    }
+    return res.json(posts);
+  });
 };
 
 PostController.delete = (req, res) => {
-    const { id } = req.params;
-    PostModel.deletePost(id, (err, result) => {
-        if (err) {
-            return res.status(500).end();
-        }
-        if (!result || result.deletedCount === 0) {
-            return res.status(404).end();
-        }
-        return res.status(200).json({message: 'Post deleted successfully'});
-    });
+  const { id } = req.params;
+  PostModel.deletePost(id, (err, result) => {
+    if (err) {
+      return res.status(500).end();
+    }
+    if (!result || result.deletedCount === 0) {
+      return res.status(404).end();
+    }
+    return res.status(200).json({ message: "Post deleted successfully" });
+  });
+};
+
+PostController.like = (req, res) => {
+  const { id } = req.params;
+
+  PostModel.likePost(id, (err, updatedPost) => {
+    if (err) return res.status(500).end();
+    if (!updatedPost) return res.status(404).end();
+  });
 };
 
 module.exports = PostController;
--- node-unit-mvc/models/post.model.js
+++ node-unit-mvc/models/post.model.js
@@ -1,49 +1,52 @@
-const mongoose = require('./connection');
+const mongoose = require("./connection");
 
 const postSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    date: { type: Date, default: Date.now },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
-    likes: { type: Number, default: 0 } 
-  }
-);
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  likes: { type: Number, default: 0 },
+});
 
-const Post = mongoose.model('posts', postSchema);
+const Post = mongoose.model("posts", postSchema);
 
 exports.createPost = (obj, next) => {
-    const post = new Post(obj);
+  const post = new Post(obj);
 
-    post.save(function(err, post) {
-        next(err, post)
-    }) 
-}
+  post.save(function (err, post) {
+    next(err, post);
+  });
+};
 
 exports.updatePost = (id, obj, next) => {
-    Post.findByIdAndUpdate(
-        id,
-        obj,
-        { new: true },
-        function(err, post) {
-            next(err, post)
-        }
-    );
-}
+  Post.findByIdAndUpdate(id, obj, { new: true }, function (err, post) {
+    next(err, post);
+  });
+};
 
 exports.findPost = (id, next) => {
-    Post.findById(id, function(err, post) {
-        next(err, post);
-    });
+  Post.findById(id, function (err, post) {
+    next(err, post);
+  });
 };
 
 exports.getAllPosts = (next) => {
-    Post.find({}, function(err, posts) {
-        next(err, posts);
-    });
+  Post.find({}, function (err, posts) {
+    next(err, posts);
+  });
 };
 
 exports.deletePost = (id, next) => {
-    Post.findByIdAndDelete(id, function(err, result) {
-        next(err, result);
-    });
+  Post.findByIdAndDelete(id, function (err, result) {
+    next(err, result);
+  });
+};
+
+exports.likePost = (id, next) => {
+  if (id === "error") return next(new Error("Server error"));
+  if (id === "notfound") return next(null, null);
 };
